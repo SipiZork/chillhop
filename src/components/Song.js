cf9e9885843d562ff36302a1887f0cf1920ae043
Mark <sipizork@gmail.com>
2021-02-13 09:28:40 +0100

    fix cover rotation

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 
+import { motion } from 'framer-motion';
+
 const Song = ({ currentSong, songInfo }) => {
 
 
   return (
     <div className="song-container">
-      <div className={`cover`} style={{ transform: `rotateZ(${songInfo.rotate}deg)`, '--color': currentSong.color[0]}}>
+      <motion.div className={`cover`} initial={{ rotate: 0 }} animate={{ rotate: songInfo.rotate, transition: {ease: "linear"}}} style={{ '--color': currentSong.color[0]}}>
         <img src={currentSong.cover} alt={currentSong.name}/>
-      </div>
+      </motion.div>
       <h2>{currentSong.name}</h2>
       <h3>{currentSong.artist}</h3>
     </div>
